d10239d9e7590607e23836a1b80cf59d32fed62b
panupvngg <165630469+panupvngg@users.noreply.github.com>
2024-04-01 20:37:52 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "Python is awesome"
+x = str(input())
+if x == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
---
+++ cell02/ex03/mult.py
@@ -0,0 +1,10 @@
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+sum = x*y
+print(x , "x" ,y , "=" ,sum)
+if sum > 0:
+    print("The result is positive.")
+if sum < 0:
+    print("The result is negative.")
+if sum == 0:
+    print("The result is positive and negative.")
